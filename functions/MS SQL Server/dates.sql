7674fd23c7db963408c1b3ec4e81ffec448b5e4d
E33605 <debmalyapramanik.005@gmail.com>
2024-06-26 15:00:10 +0530

    ✨ add function to get formatted month name from date

--- functions/MS SQL Server/dates.sql
+++ functions/MS SQL Server/dates.sql
@@ -46,3 +46,34 @@ BEGIN
     RETURN @resolvedQuarter
 END;
 GO
+
+CREATE FUNCTION [dbo].[getMonthName] (@date DATE) RETURNS VARCHAR(6) AS
+BEGIN
+    DECLARE @monthName VARCHAR(6);
+    DECLARE @_tempMonthValue INTEGER;
+
+    IF @date IS NULL
+        SET @monthName = NULL;
+    ELSE BEGIN
+        SET @_tempMonthValue = MONTH(@date);
+
+        -- Ref. Link: https://stackoverflow.com/a/1181147/6623589
+        SET @monthName = CASE
+            WHEN @_tempMonthValue = 1  THEN '01-JAN'
+            WHEN @_tempMonthValue = 2  THEN '02-FEB'
+            WHEN @_tempMonthValue = 3  THEN '03-MAR'
+            WHEN @_tempMonthValue = 4  THEN '04-APR'
+            WHEN @_tempMonthValue = 5  THEN '05-MAY'
+            WHEN @_tempMonthValue = 6  THEN '06-JUN'
+            WHEN @_tempMonthValue = 7  THEN '07-JUL'
+            WHEN @_tempMonthValue = 8  THEN '08-AUG'
+            WHEN @_tempMonthValue = 9  THEN '09-SEP'
+            WHEN @_tempMonthValue = 10 THEN '10-OCT'
+            WHEN @_tempMonthValue = 11 THEN '11-NOV'
+            WHEN @_tempMonthValue = 12 THEN '12-DEC'
+        END;
+    END;
+
+    RETURN @monthName
+END;
+GO
