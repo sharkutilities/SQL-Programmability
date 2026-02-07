b4c632df0e34ce57ff1137bc9baec07f88bdb3a0
Debmalya Pramanik <debmalyapramanik.005@gmail.com>
2026-02-07 16:42:35 +0530

    🩹 revised query to include table size information
    Updated query to retrieve table information including size.

--- scripts/Postgre SQL/tables.sql
+++ scripts/Postgre SQL/tables.sql
@@ -1,5 +1,16 @@
-SELECT * FROM INFORMATION_SCHEMA.TABLES
-WHERE table_schema NOT IN ('pg_catalog', 'information_schema')
-ORDER BY
-	table_schema
-	, table_name
+/********************************************************************
+A Short Query to get a Table Information - Name, Indexes, Size, etc.
+
+Uses the internal `pg_table` to get the table information like name,
+indexes and an internal PostgreSQL function to get the table size.
+********************************************************************/
+
+SELECT
+	*
+	, pg_size_pretty(pg_total_relation_size(
+        schemaname || '.' || tablename
+    )) AS tablesize
+FROM pg_tables WHERE schemaname NOT IN (
+    'pg_catalog', 'information_schema'
+)
+ORDER BY pg_total_relation_size(schemaname || '.' || tablename) DESC
